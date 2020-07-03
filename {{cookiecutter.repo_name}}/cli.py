da9ba4c68ff4b6d054fa901985285a94cc1cf522
abidibo <abidibo@gmail.com>
2020-07-03 17:53:37 +0200

    adds clean command

--- {{cookiecutter.repo_name}}/cli.py
+++ {{cookiecutter.repo_name}}/cli.py
@@ -26,6 +26,12 @@ if __name__ == "__main__":
         action='store_true',
         help='starts the environment')
     parser.add_argument(
+        '--clean',
+        dest='clean',
+        required=False,
+        action='store_true',
+        help='removes docker containers and volumes')
+    parser.add_argument(
         '--shell',
         dest='open_shell',
         required=False,
@@ -66,6 +72,13 @@ if __name__ == "__main__":
     # options parsing
     if args['start']:
         os.system("docker-compose -f docker-compose.yml up")
+    elif args['clean']:
+        os.system("docker container stop {{cookiecutter.repo_name}}_app")
+        os.system("docker container stop {{cookiecutter.repo_name}}_mysql")
+        os.system("docker container rm {{cookiecutter.repo_name}}_app")
+        os.system("docker container rm {{cookiecutter.repo_name}}_mysql")
+        os.system("docker volume rm {{cookiecutter.repo_name}}_app")
+        os.system("docker volume rm {{cookiecutter.repo_name}}_db-data")
     elif args['open_shell']:
         os.system("docker-compose -f docker-compose.yml exec app bash")
     elif args['createsuperuser']:
