db250f4a7df2cd4e9cdb6845358fbb91aa8bad71
Elisa Rubin <elisa.rubin@otto.to.it>
2025-06-25 10:25:17 +0200

    fix(search_app): apps.py

--- {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/search_app/apps.py
+++ {{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}/search_app/apps.py
@@ -7,6 +7,3 @@ from django.apps import AppConfig
 class SearchAppConfig(AppConfig):
     name = 'search_app'
     verbose_name = "Search"
-
-    def ready(self):
-        from . import components  # noqa
